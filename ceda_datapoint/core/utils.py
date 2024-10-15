f3028972d3f44c2fd0a7e3671f5bc070d88e3009
dwest77a <daniel.westwood@stfc.ac.uk>
2024-10-15 08:33:52 +0100

    Completed utils module

--- ceda_datapoint/core/utils.py
+++ ceda_datapoint/core/utils.py
@@ -2,11 +2,14 @@ __author__    = "Daniel Westwood"
 __contact__   = "daniel.westwood@stfc.ac.uk"
 __copyright__ = "Copyright 2024 United Kingdom Research and Innovation"
 
+# URLs for specific organisations stac catalogs
+# NOTE: In future this may be extended to intake catalogs depending on demand and similarity.
 urls = {
     'CEDA':'https://api.stac.ceda.ac.uk'
 }
 
+# Will at some point become deprecated but is currently needed for CMIP6/CCI records.
 method_format = {
-        'reference_file': 'kerchunk',
-        'reference_file_2': 'CFA',
-    }
+    'reference_file': 'kerchunk',
+    'reference_file_2': 'CFA',
+}
