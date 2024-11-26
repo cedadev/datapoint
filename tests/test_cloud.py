6940314990ed04fb584a829a91ea74d6f8f7c65c
dwest77a <daniel.westwood@stfc.ac.uk>
2024-11-26 10:56:39 +0000

    Migrated logstream to utils

--- tests/test_cloud.py
+++
@@ -1,8 +0,0 @@
-from ceda_datapoint.core.cloud import DataPointCluster
-
-def test_cluster():
-    dpc = DataPointCluster([], 'test_search',meta={})
-    assert hasattr(dpc, 'meta')
-
-if __name__ == '__main__':
-    test_cluster()
