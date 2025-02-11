e152f7915fa313358edbf22f3f81882ff383ee9f
dwest77 <daniel.westwood@stfc.ac.uk>
2025-02-11 10:40:31 +0000

    Updated imports for isort

--- ceda_datapoint/__init__.py
+++ ceda_datapoint/__init__.py
@@ -3,6 +3,4 @@ __contact__   = "daniel.westwood@stfc.ac.uk"
 __copyright__ = "Copyright 2024 United Kingdom Research and Innovation"
 
 from .core import DataPointClient
-
 from .utils import disable_logs, enable_logs
-
